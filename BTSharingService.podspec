4c7bbf49aa54f70e7e343cb5bb0c1c88d0e049fb
Borut Tomažin <borut.tomazin@icloud.com>
2013-11-18 13:47:37 +0100

    Added an example.

--- BTSharingService.podspec
+++ BTSharingService.podspec
@@ -1,15 +1,14 @@
 Pod::Spec.new do |s|
   s.name         = "BTSharingService"
-  s.version      = "1.0.0"
+  s.version      = "1.1"
   s.summary      = "Nice and simple sharing service."
   s.homepage     = "https://github.com/borut-t/BTSharingService"
-  s.screenshots  = "https://raw.github.com/borut-t/BTButton/master/Screenshots/preview.png"
   s.license      = { :type => 'zlib', :file => 'LICENCE.md' }
-  s.author       = { "Borut Tomažin" => "borut.tomazin@gmail.com" }
+  s.author       = { "Borut Tomažin" => "borut.tomazin@icloud.com" }
   s.platform     = :ios, '5.0'
-  s.source       = { :git => "https://github.com/borut-t/BTSharingService.git", :tag => "1.0.0" }
+  s.source       = { :git => "https://github.com/borut-t/BTSharingService.git", :tag => "1.1" }
   s.source_files = 'BTSharingService', 'Facebook'
-  s.frameworks   = 'UIKit', 'Foundation', 'CoreGraphics'
+  s.frameworks   = 'Twitter', 'MessageUI', 'Social'
   s.requires_arc = true
 end
  
--- Sample App/BTSharingServiceExample/ViewController.m
+++ Sample App/BTSharingServiceExample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "BTSharingService.h"
 
 @interface ViewController ()
 
@@ -24,6 +25,29 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)share:(id)sender
+{
+    //Set language service uses instead system language
+    //[[BTSharingService sharedInstance] setPreferredLanguage:@"sl"];
+    
+    BTSharingServiceType sharingType = 0;
+    switch ([(UIButton *)sender tag]) {
+        case 1: sharingType = BTSharingServiceTypeFacebook; break;
+        case 2: sharingType = BTSharingServiceTypeTwitter; break;
+        case 3: sharingType = BTSharingServiceTypeMail; break;
+        case 4: sharingType = BTSharingServiceTypeiMessage; break;
+        case 5: sharingType = BTSharingServiceTypeSafari; break;
+    }
+    
+    if (sharingType) {
+    [[BTSharingService sharedInstance] shareWithType:sharingType
+                                             subject:@"Example"
+                                                body:@"Example body text."
+                                                 url:[NSURL URLWithString:@"http://www.apple.com"] recipients:nil
+                                    onViewController:self];
+    }
 }
 
 @end
